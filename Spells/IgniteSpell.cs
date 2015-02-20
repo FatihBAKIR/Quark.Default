826a19896aaf321a1dcb9c3cc458d06d69a7fbf1
Fatih BAKIR <fatih@linux.com>
2015-02-20 14:48:04 +0200

    Ignite fix

--- Spells/IgniteSpell.cs
+++ Spells/IgniteSpell.cs
@@ -5,12 +5,6 @@ using Quark.Targeting;
 
 public class IgniteSpell : Spell
 {
-	public override string Name {
-		get {
-			return "Ignite";
-		}
-	}
-
 	public override TargetMacro TargetMacro {
 		get {
 			return new NearestCharacter(5);
@@ -20,7 +14,7 @@ public class IgniteSpell : Spell
 	protected override EffectCollection TargetingDoneEffects {
 		get {
 			return new EffectCollection {
-				new BuffEffect(new DoTBuff())
+				new BuffEffect(new DoTBuff(1, 10, 10))
 			};
 		}
 	}
